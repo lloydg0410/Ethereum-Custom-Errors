5b867d84fcd8dd9b07229ea5a80ed70d7148e944
lloydg0410 <lloyd.g.0410@outlook.com>
2025-12-10 01:59:46 -0800

    complete calculation the custom error selectors

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Start a typescript project!");
+import { keccak256, toUtf8Bytes } from "ethers";
+import CustomErrors from "./data/custom-errors.json"
+import fs from "fs";
+
+const main = () => {
+  const errorSelectors: Record<string, string> = {};
+
+  CustomErrors.map((error) => {
+    const selector = keccak256(toUtf8Bytes(error)).slice(0, 10);
+    errorSelectors[selector] = error;
+  });
+
+  fs.writeFileSync("./src/data/custom-error-selectors.json", JSON.stringify(errorSelectors, null, 2));
+  console.log("Complete calculating the error selectors for all custom errors");
+}
+
+main();
